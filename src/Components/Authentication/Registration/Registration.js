bf0e951290b30db29ca56502db80ee315311e724
Johirul Islam Nishat <jahirulislamn@gmail.com>
2021-11-15 11:21:29 +0600

    registration update

--- src/Components/Authentication/Registration/Registration.js
+++ src/Components/Authentication/Registration/Registration.js
@@ -12,7 +12,7 @@ const Registration = () => {
     const [user, setUser] = useState({});
 
     const [signupData, setSignupData] = useState({});
-    const { authError, signUpUsingEmail, isLoading } = useAuth();
+    const { authError, signUpUsingEmail, isLoading, signInUsingGoogle, saveUser } = useAuth();
 
     // redirect 
     const location = useLocation();
@@ -43,6 +43,25 @@ const Registration = () => {
         alert('Registration Success!!!')
         e.preventDefault();
     }
+
+    //ggl login 
+    const handleGoogleLogin = () => {
+        signInUsingGoogle(location, history)
+            .then(result => {
+
+                //redirect
+                history.push(redirect_uri);
+
+                const user = result.user;
+                saveUser(user.email, user.displayName, 'PUT');
+
+                //session storage
+                setUser(result.user);
+                sessionStorage.setItem("email", result.user.email);
+                // console.log(result.user);
+            })
+    }
+
     return (
         <>
             <Header />
@@ -63,7 +82,7 @@ const Registration = () => {
                                     </svg>
                                     Facebook
                                 </button>
-                                <button type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                <button onClick={handleGoogleLogin} type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                                     <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z">
                                         </path>
